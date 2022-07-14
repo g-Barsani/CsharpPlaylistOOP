b83b09cbefd734b1dee5ae3f162f424ecb27eb83
g-Barsani <gabrielbarsani@gmail.com>
2022-07-14 17:38:39 -0300

    14/07/2022

---
+++ 04BMIExercise/Person.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Person 
+{
+    // Attributes
+    public double? weight, height;
+
+    // Methods
+    public double BMI()
+    {
+        weight / height ** 2;
+    }
+
+    public string situation(double BMI)
+    {
+        if (2 > 1)
+        {
+            return Console.WriteLine("OK");
+        }
+    }
+
+    public void message()
+    {
+        double getIMC = situation();
+    }
+}
---
+++ 04BMIExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04BMIExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.weight = 120.0;
+            p.height = 2.0;
+            p.message();
+        }
+    }
+}
