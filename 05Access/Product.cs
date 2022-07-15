f277e198611ef2a0774d1b5b33f82869cd10b342
g-Barsani <gabrielbarsani@gmail.com>
2022-07-15 12:02:50 -0300

    05Acess

---
+++ 05Access/Product.cs
@@ -0,0 +1,13 @@
+class Product
+{
+
+    /*
+        Access Modifiers:
+        public -> Attributes and methods accessible from all classes
+        private -> Attributes and methods are only accessible within the same class
+        protected -> Attributes and methods accesible within the same class, or in a class that is inherited from that class
+    */
+
+    public string name;
+    private double value;
+}
---
+++ 05Access/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _05Access
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            // p.
+        }
+    }
+}
