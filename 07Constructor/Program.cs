c90262c0a0e3b90ae8bcddd813dae1ccfc6ce774
g-Barsani <gabrielbarsani@gmail.com>
2022-07-15 15:38:14 -0300

    .

---
+++ 07Constructor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+        }
+    }
+}
